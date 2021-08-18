14a3264db38200c7fd2f7cd46eeb755713aaf38f
Konstantyn-Tokar <79169268+Konstantyn-Tokar@users.noreply.github.com>
2021-08-18 17:53:05 +0300

    pnotify
    разобрался pnotify

--- src/index.js
+++ src/index.js
@@ -3,41 +3,80 @@ console.log('Ukraine');
 import fetchCountries from './fetchCountries';
 import countryListMarkup from './templates/countriesList.hbs';
 import сardСountryMarkup from './templates/сardСountry.hbs';
-//добавил debounce
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/desktop/dist/PNotifyDesktop';
+import '@pnotify/core/dist/BrightTheme.css';
+import { alert, notice, info, success, error } from '@pnotify/core';
+
+// error({
+//   title: `Too many matches found.`,
+//   text: `We found  countries. Please enter a more specific query!`,
+//   styling: 'brighttheme',
+//   delay: 2000,
+// });
 const debounce = require('lodash.debounce');
+//добавил debounce
 
 //создать рефы
 const refs = {
   inputCountry: document.querySelector('#country-name'),
   listCountry: document.querySelector('#country-list'),
 };
-console.log(refs.listCountry);
-console.log(refs.inputCountry);
 
 //повесить на инпут слушатель (input)
 refs.inputCountry.addEventListener('input', debounce(onInput, 500));
-
-// console.log(input);
-
 //написать функцию которая ходит на сервер и забирает список стран
-
 function onInput(e) {
   const country = e.target.value;
   fetchCountries(country)
-    .then(r => makeTheMarkup(r))
-    .catch(() => console.log('Жопа'));
+    .then(r => doTheAnswer(r))
+    .catch(() => errorServerMessage());
 }
 
 //написать функция рендеринга HTML при .then
 
-function makeTheMarkup(countries) {
-  console.log(countries.length);
+function doTheAnswer(countries) {
   if (countries.length === 1) {
     refs.listCountry.innerHTML = сardСountryMarkup(countries);
   }
-  if (countries.length >= 2 && countries.length < 10) {
+  if (countries.length >= 2 && countries.length <= 10) {
     refs.listCountry.innerHTML = countryListMarkup(countries);
+  }
+  if (countries.length > 10) {
+    info({
+      title: 'Внимание',
+      text: 'Пожалуйста, введите более конкретный запрос!',
+      delay: 2000,
+    });
+  }
+  if (countries.status === 404) {
+    resetInput();
+    errorMessage();
   }
 }
 
-//написать функция отображающею ошибку при .catch
+//написать функция отображающею ошибки
+
+function errorServerMessage() {
+  refs.listCountry.innerHTML = '';
+  error({
+    title: 'Ошибка',
+    text: 'Проблемы с сервером , повоторите попЫтку позже',
+    delay: 2000,
+  });
+}
+
+function errorMessage() {
+  refs.listCountry.innerHTML = '';
+  error({
+    title: 'Ошибка',
+    text: 'По вашему запросу ничего не найдено',
+    delay: 2000,
+  });
+}
+
+function resetInput() {
+  setTimeout(() => {
+    refs.inputCountry.value = '';
+  }, 2000);
+}
